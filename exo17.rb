8cf5f9031ba6b581f4be09d57ac5f6f28fdf56a1
Charleneripaldi <77022043+Charleneripaldi@users.noreply.github.com>
2021-01-13 10:10:44 +0100

    Update exo17.rb

--- exo17.rb
+++ exo17.rb
@@ -1,11 +1,13 @@
+puts "Bienvenue dans la super pyramide, rentres un nombre entre 0 et 25 ?"
+print ">"
+x = gets.chomp.to_i
+return if x < 0|| x > 25 
+puts "voici la pyramide !"
+i = 1
 
 
-x = 3
 while x > 0 do
-  puts x 
+  puts " " * x + "#" * i
+  i += 2
   x -= 1
 end
-
-#for x in [ 1, 5, 6, 15 ] do 
- # puts x 
-#end
